52e2e8878b12d6e649fda4549309b047335be55b
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-03-26 01:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (98.08%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,32 +1,28 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n=image.length;
-        int m=image[0].length;
-        int a=image[sr][sc];
-        if(a==color){
-            return image;
-        }
-        image[sr][sc]=color;
-
-        Queue<int[]> q= new LinkedList<>();
-        q.add(new int[]{sr,sc});
-
-        int[] r={0,-1,0,1};
-        int[] c={-1,0,1,0};
-        while(!q.isEmpty()){
-            int[] b=q.poll();
-            int row=b[0];
-            int col=b[1];
-            for(int i=0;i<4;i++){
-                int x=row+r[i];
-                int y=col+c[i];
-                if(x>=0 && x<n && y>=0 && y<m && image[x][y]==a){
-                    image[x][y]=color;
-                    q.add(new int[]{x,y});
-                }
-            }
+        int originalColor = image[sr][sc];
+        
+        // Edge case
+        if (originalColor == color) return image;
+        
+        dfs(image, sr, sc, originalColor, color);
+        return image;
+    }
+    
+    private void dfs(int[][] image, int r, int c, int originalColor, int color) {
+        // Boundary + condition check
+        if (r < 0 || c < 0 || r >= image.length || c >= image[0].length 
+            || image[r][c] != originalColor) {
+            return;
         }
         
-        return image;
+        // Fill color
+        image[r][c] = color;
+        
+        // 4 directions
+        dfs(image, r + 1, c, originalColor, color);
+        dfs(image, r - 1, c, originalColor, color);
+        dfs(image, r, c + 1, originalColor, color);
+        dfs(image, r, c - 1, originalColor, color);
     }
 }
