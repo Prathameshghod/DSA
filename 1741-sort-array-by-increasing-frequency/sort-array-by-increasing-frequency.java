3844552f9375a2865c2ca1cd10f28f29089901ff
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-05-09 22:25:03 +0530

    Time: 9 ms (48.12%) | Memory: 45.9 MB (24.08%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.java
@@ -1,24 +1,40 @@
-class Solution {
-    public int[] frequencySort(int[] nums) {
-        int n=nums.length;
-        Map<Integer,Integer> a= new HashMap<>();
-        for(int i=0;i<n;i++){
-            a.put(nums[i],a.getOrDefault(nums[i],0)+1);
-        }
-        Integer[] b=new Integer[n];
-         for(int i=0;i<n;i++){
-            b[i]=nums[i];
-         }
-         Arrays.sort(b,(x,y) -> {
-            if (a.get(x).equals(a.get(y))){
-                return Integer.compare(y,x);
-            }
-            return Integer.compare(a.get(x),a.get(y));
-         });
+import java.util.*;
 
-         for(int i=0;i<n;i++){
-            nums[i]=b[i];
-         }
-         return nums;
+class Solution {
+
+    public int[] frequencySort(int[] nums) {
+
+        Map<Integer, Integer> a = new HashMap<>();
+
+        // frequency count
+        for (int x : nums) {
+            a.put(x, a.getOrDefault(x, 0) + 1);
+        }
+
+        // store numbers in list
+        List<Integer> b = new ArrayList<>();
+
+        for (int x : nums) {
+            b.add(x);
+        }
+
+        // sort list
+        b.sort((x, y) -> {
+
+            // same frequency -> larger number first
+            if (a.get(x).equals(a.get(y))) {
+                return Integer.compare(y, x);
+            }
+
+            // smaller frequency first
+            return Integer.compare(a.get(x), a.get(y));
+        });
+
+        // copy back to array
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = b.get(i);
+        }
+
+        return nums;
     }
 }
