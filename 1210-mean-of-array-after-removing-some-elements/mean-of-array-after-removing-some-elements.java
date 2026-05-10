aa0b55276e66fa5457aab1c122a7a4ca457c5443
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-05-10 11:11:12 +0530

    Time: 24 ms (6.94%) | Memory: 48.1 MB (6.00%) - LeetSync

--- 1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.java
+++ 1210-mean-of-array-after-removing-some-elements/mean-of-array-after-removing-some-elements.java
@@ -1,24 +1,44 @@
 class Solution {
     public double trimMean(int[] arr) {
         int n=arr.length;
-        int b=(5*n)/100;
-        int c=n-b-b;
-        int d=0;
-        Arrays.sort(arr);
-        for(int i=b;i<n-b;i++){
-            d+=arr[i];
+        int b=n/20;
+        TreeMap<Integer,Integer> t= new TreeMap<>();
+        for(int i=0;i<n;i++){
+           t.put(arr[i],t.getOrDefault(arr[i],0)+1);
         }
-        // System.out.println(n);
-        // for(int i=0;i<n;i++){
-        //     System.out.print(arr[i]+" ");
-        // }
-        // System.out.println("sdded");
-        // for(int i=b;i<n-b;i++){
-        //     System.out.print(arr[i]+" ");
-        // }
-        // System.out.println(d);
-        // System.out.println(c);
-        double e=(double) d/c;
-        return e;
+        int k=b;
+        System.out.println(t);
+        int c=0;
+        for(int i:arr){
+            c+=i;
+        }
+        while(k>0){
+            int a=t.firstKey();
+            int x=t.get(a);
+            int d=Math.min(k,x);
+            c-=(d*a);
+            if(d==x){
+                t.remove(a);
+            }
+            else{
+                t.put(a,x-d);
+            }
+            k-=d;
+        }
+        k=b;
+        while(k>0){
+            int a=t.lastKey();
+            int x=t.get(a);
+            int d=Math.min(k,x);
+            c-=(d*a);
+            if(d==x){
+                t.remove(a);
+            }
+            else{
+                t.put(a,x-d);
+            }
+            k-=d;
+        }
+        return (double)c/(n-2*b);
     }
 }
