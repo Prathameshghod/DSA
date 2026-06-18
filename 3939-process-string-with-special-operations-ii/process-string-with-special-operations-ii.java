6b3f14a2ed06d1427f37f8775d43eec1901622c7
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-06-18 19:54:58 +0530

    Time: 25 ms (99.25%) | Memory: 48.2 MB (7.52%) - LeetSync

--- 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.java
@@ -1,54 +1,41 @@
 class Solution {
+
+    private boolean validK(long k, long size) {
+        return k >= 0 && k < size;
+    }
+
     public char processStr(String s, long k) {
-        int n=s.length();
-        long l=0;
-        for(int i=0;i<n;i++){
-            char a=s.charAt(i);
-            switch(a){
-                case '*':
-                    if(l>0){
-                        l--;
-                    }
-                    break;
-                case '#':
-                    l*=2;
-                    break;
-                case '%':
-                    break;
-                default:
-                    l++;
-                    break;
+
+        long size = 0;
+        char[] chars = s.toCharArray();
+
+        for (char c : chars) {
+            if (c == '*') { 
+                if (size > 0) --size; 
+            } else if (c == '#') { 
+                size *= 2;
+            } else if (c != '%') ++size; 
+        }
+
+        if (!validK(k, size)) return '.';
+
+        for (int i = chars.length - 1; i >= 0; --i) {
+            char c = chars[i];
+            if (c == '*') {
+                ++size;
+            } else if (c == '#') {
+                size = size / 2;
+                if (k >= size) k = k % size;
+            } else if (c == '%') {
+                k = size - k - 1;
+            } else {
+                if (k == size - 1) return c;
+                --size;
             }
         }
-        if(k+1>l){
-            return '.';
-        }
-        
-        for(int i=n-1;i>=0;i--){
-            char c=s.charAt(i);
-            switch(c){
-                case '*':
-                    l++;
-                    break;
-                
-                case '%':
-                    k=l-k-1;
-                    break;
-                
-                case '#':
-                    if(k+1>(l+1)/2){
-                        k-=l/2;
-                    }
-                    l=(l+1)/2;
-                    break;
-                default:
-                    if(k+1==l){
-                        return c;
-                    }
-                    l--;
-                    break;
-            }
-        }
+
         return '.';
+
+        
     }
 }
