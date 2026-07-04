a14a92a447b4e482975c197f78ec2d0a2e3c6c51
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-07-04 18:52:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (46.94%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
@@ -9,17 +9,18 @@ class Solution {
         for(int j=0;j<n;j++){
             char i=s.charAt(j);
             if( f[i-'a']>-1){
+                l[i-'a']=j;
                 continue;
             }
              f[i-'a']=j;
         }
-        for(int j=n-1;j>=0;j--){
-            char i=s.charAt(j);
-            if( l[i-'a']>-1){
-                continue;
-            }
-             l[i-'a']=j;
-        }
+        // for(int j=n-1;j>=0;j--){
+        //     char i=s.charAt(j);
+        //     if( l[i-'a']>-1){
+        //         continue;
+        //     }
+        //      l[i-'a']=j;
+        // }
         for(int i=0;i<26;i++){
             if(f[i]==-1 || l[i]==-1 ){
                 continue;
