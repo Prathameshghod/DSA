78b2d220adfeb6dca1a680be560771370220474c
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-07-10 22:39:28 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (31.27%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.java
+++ 1217-relative-sort-array/relative-sort-array.java
@@ -1,36 +1,38 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int n=arr1.length;
-        Map<Integer,Integer> m= new HashMap<>();
-        for(int i=0;i<n;i++){
-            m.put(arr1[i],m.getOrDefault(arr1[i],0)+1);
-        }
-        int idx=0;
-        for(int i:arr2){
-            int f=m.get(i);
-            while(f-->0){
-                arr1[idx++]=i;
-            }
-            m.remove(i);
+        int n = arr1.length;
+        int m = arr2.length;
+        int max = -1;
+        
+        // Find the maximum value to size the frequency array
+        for (int i = 0; i < n; i++) {
+            max = Math.max(arr1[i], max);
         }
         
-
-        List<Integer> rem = new ArrayList<>();
-
-        for (int key : m.keySet()) {
-
-            int f = m.get(key);
-
-            while (f-- > 0) {
-                rem.add(key);
+        // Build the frequency array
+        int[] a = new int[max + 1];
+        for (int i = 0; i < n; i++) { // Fixed: loop up to n
+            a[arr1[i]]++;
+        }
+        
+        int j = 0; // Pointer to overwrite elements in arr1
+        
+        // Step 1: Place elements in the relative order of arr2
+        for (int i = 0; i < m; i++) {
+            while (a[arr2[i]] > 0) {
+                arr1[j++] = arr2[i];
+                a[arr2[i]]--;
             }
         }
-
-        Collections.sort(rem);
-
-        for (int num : rem) {
-            arr1[idx++] = num;
+        
+        // Step 2: Place the remaining elements in ascending order
+        for (int i = 0; i <= max; i++) {
+            while (a[i] > 0) {
+                arr1[j++] = i;
+                a[i]--;
+            }
         }
+        
         return arr1;
     }
 }
