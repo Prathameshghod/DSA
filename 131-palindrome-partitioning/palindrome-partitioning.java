553f7e57ffd5e4016350bc7a51ab8c364c58aff5
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-07-20 00:11:14 +0530

    Time: 8 ms (75.32%) | Memory: 65.2 MB (58.01%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,5 +1,8 @@
 class Solution {
-    private boolean ispal(String s, int low, int high) {
+    private boolean ispal(String s, int low, int high,Boolean[][] t) {
+        if(t[low][high]!=null){
+            return t[low][high];
+        }
         while (low < high) {
             if (s.charAt(low++) != s.charAt(high--)) {
                 return false;
@@ -7,24 +10,26 @@ class Solution {
         }
         return true;
     }
-    public void solve(String s,int idx,List<List<String>> a, List<String> b){
+    public void solve(String s,int idx,List<List<String>> a, List<String> b,Boolean[][] t){
         if(idx==s.length()){
             a.add(new ArrayList<>(b));
             return;
         }
 
         for(int i=idx;i<s.length();i++){
-            if(ispal(s,idx,i)){
+            if(ispal(s,idx,i,t)){
                 b.add(s.substring(idx,i+1));
-                solve(s,i+1,a,b);
+                solve(s,i+1,a,b,t);
                 b.remove(b.size()-1);
             }
         }
     }
     public List<List<String>> partition(String s) {
+        int n=s.length();
         List<List<String>> a= new ArrayList<>();
         List<String> b= new ArrayList<>();
-        solve(s,0,a,b);
+        Boolean[][] t= new Boolean[n+1][n+1];
+        solve(s,0,a,b,t);
         return a;
     }
 }
