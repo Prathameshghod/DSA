a98975c77b280994a1a943e80d867a7e4d04d405
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-07-23 13:51:40 +0530

    Time: 344 ms (15.46%) | Memory: 237.1 MB (13.10%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,29 +1,24 @@
-    class Solution {
-        public int solve(int[] a,int i,int prev,int[][] t){
-            if(i==a.length){
-                return 0;
-            }
-            if(prev!=-1 && t[i][prev]!=-1){
-                return t[i][prev];
-            }
-            int b=0;
-            if(prev==-1 || a[i]>a[prev]){
-                b=1+solve(a,i+1,i,t);
-            }
-            int c=solve(a,i+1,prev,t);
-
-            if(prev!=-1){
-                t[i][prev]=Math.max(b,c);
-            }
-            return Math.max(b,c);
-            
+class Solution {
+    public int solve(int[] a,int curr,int prev,int[][] t){
+        if(curr==a.length){
+            return 0;
         }
-        public int lengthOfLIS(int[] nums) {
-            int n=nums.length;
-            int[][] t= new int[n+1][n+1];
-            for(int[] i:t){
-                Arrays.fill(i,-1);
-            }
-            return solve(nums,0,-1,t);
+        if(t[curr][prev+1]!=-1){
+            return t[curr][prev+1];
+        }
+        if(prev==-1 || a[curr]>a[prev]){
+            return t[curr][prev+1]=Math.max(1+solve(a,curr+1,curr,t),solve(a,curr+1,prev,t));
+        }
+        else{
+            return t[curr][prev+1]=solve(a,curr+1,prev,t);
         }
     }
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int[][] t=new int[n+1][n+1];
+        for(int[] i:t){
+            Arrays.fill(i,-1);
+        }
+        return solve(nums,0,-1,t);
+    }
+}
