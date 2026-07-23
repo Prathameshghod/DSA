aab87e916749c061f3b5539b4e55e39ababd55a0
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-07-23 13:05:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (36.00%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,11 +1,16 @@
 class Solution {
+    public int solve(int n,int[] t){
+        if(n==1 || n==0){
+            return 1;
+        }
+        if(t[n]!=-1){
+            return t[n];
+        }
+        return t[n]=solve(n-1,t)+solve(n-2,t);
+    }
     public int climbStairs(int n) {
         int[] t= new int[n+1];
         Arrays.fill(t,-1);
-        t[0]=t[1]=1;
-        for(int i=2;i<=n;i++){
-            t[i]=t[i-1]+t[i-2];
-        }
-        return t[n];
+        return solve(n,t);
     }
 }
