d4622764c880d8757b75a5346c97e6c5236a193d
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-07-25 10:23:59 +0530

    Time: 8 ms (74.77%) | Memory: 65.4 MB (40.98%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,35 +1,35 @@
+
 class Solution {
-    private boolean ispal(String s, int low, int high,Boolean[][] t) {
-        if(t[low][high]!=null){
-            return t[low][high];
-        }
-        while (low < high) {
-            if (s.charAt(low++) != s.charAt(high--)) {
-                return false;
+    int n;
+     public boolean ispal(String s,int i,int j){
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
+               return false;
             }
+            i++;
+            j--;
         }
         return true;
     }
-    public void solve(String s,int idx,List<List<String>> a, List<String> b,Boolean[][] t){
-        if(idx==s.length()){
-            a.add(new ArrayList<>(b));
-            return;
+    public void solve(String s,int idx,List<List<String>> a,List<String> b){
+        if(idx==n){
+             a.add(new ArrayList<>(b));
+             return;
         }
-
-        for(int i=idx;i<s.length();i++){
-            if(ispal(s,idx,i,t)){
+        for(int i=idx;i<n;i++){
+            if(ispal(s,idx,i)){
                 b.add(s.substring(idx,i+1));
-                solve(s,i+1,a,b,t);
+                solve(s,i+1,a,b);
                 b.remove(b.size()-1);
             }
         }
     }
     public List<List<String>> partition(String s) {
-        int n=s.length();
+        n=s.length();
         List<List<String>> a= new ArrayList<>();
         List<String> b= new ArrayList<>();
-        Boolean[][] t= new Boolean[n+1][n+1];
-        solve(s,0,a,b,t);
+        
+        solve(s,0,a,b);
         return a;
     }
 }
