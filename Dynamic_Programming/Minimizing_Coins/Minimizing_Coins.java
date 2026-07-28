8e252ac22a8a9f04f1d239b5451c716c835dcc06
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-07-28 09:58:51 +0530

    Add Java solution for Minimizing_Coins

---
+++ Dynamic_Programming/Minimizing_Coins/Minimizing_Coins.java
@@ -0,0 +1,46 @@
+/*
+ * Problem Name: Minimizing_Coins
+ * Language: Java
+ * Category: Dynamic_Programming
+ * Date: 2026-07-28
+ */
+
+/******************************************************************************
+ 
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+ 
+*******************************************************************************/
+import java.util.*;
+ 
+public class Main
+{
+    public static int solve(int s,int n,int[] a){
+        if(s==0){
+            return 0;
+        }
+        if(n==0){
+            return Integer.MAX_VALUE;
+        }
+        if(a[n-1]<=s){
+            return Math.min(1+solve(s-a[n-1],n,a),solve(s,n-1,a));
+        }
+        else{
+            return solve(s,n-1,a);
+        }
+    }
+    public static int func(int n,int s,int[] a){
+        return solve(s,n,a);
+    }
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int s=sc.nextInt();
+		int[] a= new int[n];
+		for(int i=0;i<n;i++){
+		    a[i]=sc.nextInt();
+		}
+		System.out.println(func(n,s,a));
+	}
+}
