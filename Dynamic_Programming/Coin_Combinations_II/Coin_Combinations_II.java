586e2c8bf37a8049c7d3279cc03efb294e15be6d
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-07-29 09:51:37 +0530

    Add Java solution for Coin_Combinations_II

---
+++ Dynamic_Programming/Coin_Combinations_II/Coin_Combinations_II.java
@@ -0,0 +1,47 @@
+/*
+ * Problem Name: Coin_Combinations_II
+ * Language: Java
+ * Category: Dynamic_Programming
+ * Date: 2026-07-29
+ */
+
+/******************************************************************************
+ 
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+ 
+*******************************************************************************/
+import java.util.*;
+ 
+public class Main
+{
+    public static int func(int n,int s,int[] a){
+        int[][] t= new int[n+1][s+1];
+        for(int i=0;i<n+1;i++){
+            t[i][0]=1;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<s+1;j++){
+                if(a[i-1]<=j){
+                    t[i][j]=t[i][j-a[i-1]]+t[i-1][j];
+                }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+        return t[n][s];
+        
+    }
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		int x=sc.nextInt();
+		int[] a= new int[n];
+		for(int i=0;i<n;i++){
+		    a[i]=sc.nextInt();
+		}
+		System.out.println(func(n,x,a));
+	}
+}
