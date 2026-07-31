cad47418db1107557bb278e3d1a070517e6fdb6b
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-07-31 09:45:58 +0530

    Add Java solution for Distinct_Numbers

---
+++ Sorting_and_Searching/Distinct_Numbers/Distinct_Numbers.java
@@ -0,0 +1,34 @@
+/*
+ * Problem Name: Distinct_Numbers
+ * Language: Java
+ * Category: Sorting_and_Searching
+ * Date: 2026-07-31
+ */
+
+/******************************************************************************
+ 
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+ 
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+    public static int func(int n,int[] a){
+        Set<Integer> b=new HashSet<>();
+        for(int i:a){
+            b.add(i);
+        }
+        return b.size();
+    }
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] a= new int[n];
+		for(int i=0;i<n;i++){
+		    a[i]=sc.nextInt();
+		}
+		System.out.println(func(n,a));
+	}
+}
