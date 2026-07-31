1d8235cab7bd3b4d7c3073936029c2762f57e891
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-07-31 09:52:09 +0530

    Add Java solution for Distinct_Numbers

--- Sorting_and_Searching/Distinct_Numbers/Distinct_Numbers.java
+++ Sorting_and_Searching/Distinct_Numbers/Distinct_Numbers.java
@@ -5,30 +5,28 @@
  * Date: 2026-07-31
  */
 
-/******************************************************************************
- 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
- 
-*******************************************************************************/
 import java.util.*;
-public class Main
-{
-    public static int func(int n,int[] a){
-        Set<Long> b=new HashSet<>();
-        for(int i:a){
-            b.add((long) i);
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        int[] a = new int[n];
+ 
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
         }
-        return b.size();
+ 
+        Arrays.sort(a);
+ 
+        int cnt = 1;
+        for (int i = 1; i < n; i++) {
+            if (a[i] != a[i - 1]) {
+                cnt++;
+            }
+        }
+ 
+        System.out.println(cnt);
     }
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		int n=sc.nextInt();
-		int[] a= new int[n];
-		for(int i=0;i<n;i++){
-		    a[i]=sc.nextInt();
-		}
-		System.out.println(func(n,a));
-	}
 }
