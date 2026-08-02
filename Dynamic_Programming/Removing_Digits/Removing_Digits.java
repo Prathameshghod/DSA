ff16747d6dc89306cba4e28bffed7eeaad46f473
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-08-02 20:33:24 +0530

    Add Java solution for Removing_Digits

--- Dynamic_Programming/Removing_Digits/Removing_Digits.java
+++ Dynamic_Programming/Removing_Digits/Removing_Digits.java
@@ -2,7 +2,7 @@
  * Problem Name: Removing_Digits
  * Language: Java
  * Category: Dynamic_Programming
- * Date: 2026-07-29
+ * Date: 2026-08-02
  */
 
 /******************************************************************************
