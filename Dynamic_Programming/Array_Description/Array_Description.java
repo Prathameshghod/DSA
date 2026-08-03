ce195c84c8d577f73859ba46e3b4ca481abee6f6
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-08-03 11:04:29 +0530

    Add Java solution for Array_Description

--- Dynamic_Programming/Array_Description/Array_Description.java
+++ Dynamic_Programming/Array_Description/Array_Description.java
@@ -15,33 +15,41 @@ Write your code in this editor and press "Run" button to execute it.
 import java.util.*;
 public class Main
 {
-    public static int solve(int idx,int n,int x,int[] a,int prev){
+    private static final int MOD = 1_000_000_007;
+    public static int solve(int idx,int n,int x,int[] a,int prev,int[][] t){
         if(idx==n){
             return 1;
         }
+        if(t[idx][prev]!=-1){
+            return t[idx][prev];
+        }
         if(a[idx]!=0){
             if(prev==0 || Math.abs(a[idx]-prev)<=1){
-                return solve(idx+1,n,x,a,a[idx]);
+                return t[idx][prev]=solve(idx+1,n,x,a,a[idx],t);
             }
             else{
-                return 0;
+                return t[idx][prev]=0;
             }
         }
         int b=0;
         if(idx==0 || prev==0){
             for(int i=1;i<=x;i++){
-                b+=solve(idx+1,n,x,a,i);
+                b+=solve(idx+1,n,x,a,i,t);
             }
         }
         else{
             for(int i=prev-1;i<=prev+1;i++){
-                b+=solve(idx+1,n,x,a,i);
+                b+=solve(idx+1,n,x,a,i,t);
             }
         }
-        return b;
+        return t[idx][prev]=b;
     }
     public static int func(int n,int x,int[] a){
-        return solve(0,n,x,a,0);
+        int[][] t= new int[n+1][x+1];
+        for(int[] i:t){
+            Arrays.fill(i,-1);
+        }
+        return solve(0,n,x,a,0,t)%MOD;
     }
 	public static void main(String[] args) {
 	    Scanner sc= new Scanner(System.in);
