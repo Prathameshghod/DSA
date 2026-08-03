3f108277a5b0c8da1f5190f3bd78b4d7ee0f7b96
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-08-03 10:47:23 +0530

    Add Java solution for Array_Description

---
+++ Dynamic_Programming/Array_Description/Array_Description.java
@@ -0,0 +1,59 @@
+/*
+ * Problem Name: Array_Description
+ * Language: Java
+ * Category: Dynamic_Programming
+ * Date: 2026-08-03
+ */
+
+/******************************************************************************
+ 
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+ 
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+    public static int solve(int idx,int n,int x,int[] a,int prev){
+        if(idx==n){
+            return 1;
+        }
+        if(a[idx]!=0){
+            if(prev==0 || Math.abs(a[idx]-prev)<=1){
+                return solve(idx+1,n,x,a,a[idx]);
+            }
+            else{
+                return 0;
+            }
+        }
+        int b=0;
+        if(idx==0 || prev==0){
+            for(int i=1;i<=x;i++){
+                b+=solve(idx+1,n,x,a,i);
+            }
+        }
+        else{
+            for(int i=prev-1;i<=prev+1;i++){
+                b+=solve(idx+1,n,x,a,i);
+            }
+        }
+        return b;
+    }
+    public static int func(int n,int x,int[] a){
+        return solve(0,n,x,a,0);
+    }
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int x=sc.nextInt();
+	    int[] a=new int[n];
+	    for(int i=0;i<n;i++){
+	        a[i]=sc.nextInt();       
+	    }
+	   // int n=3;
+	   // int x=5;
+	   // int[] a={2,0,2};
+	    System.out.println(func(n,x,a));
+	}
+}
