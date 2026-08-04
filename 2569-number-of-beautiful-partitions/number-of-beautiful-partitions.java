b193ab04d2c3040b3e03db8654d1668a7828d0ff
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-08-04 19:15:58 +0530

    Time: 13 ms (94.55%) | Memory: 56.1 MB (10.91%) - LeetSync

--- 2569-number-of-beautiful-partitions/number-of-beautiful-partitions.java
+++ 2569-number-of-beautiful-partitions/number-of-beautiful-partitions.java
@@ -2,37 +2,35 @@ class Solution {
     private static final int MOD = 1_000_000_007;
     public int beautifulPartitions(String s, int k, int minLength) {
         int n=s.length();
+
         if (!prime(s.charAt(0)) || prime(s.charAt(s.length() - 1))) {
             return 0;
         }
-        int[][] t=new int[k+1][n+1];
-        for(int[] i:t){
-            Arrays.fill(i,-1);
+        boolean[] isCut = new boolean[n + 1];
+        isCut[0] = true;
+        isCut[n] = true;
+        for (int i = 1; i < n; i++) {
+            if (!prime(s.charAt(i - 1)) && prime(s.charAt(i))) {
+                isCut[i] = true;
+            }
         }
-        return solve(s,k,minLength,0,t);
-    }
-    public int solve(String s, int k, int len,int idx,int[][] t){
-        int n=s.length();
-        if(k==0){
-            return idx==n?1:0;
-        }
-        if(n-idx<k*len){
-            return 0;
-        }
-        if(t[k][idx]!=-1){
-            return t[k][idx];
-        }
-        long r=0;
-        for(int i=idx+len-1;i<n;i++){
-            if(!prime(s.charAt(i))){
-                int next=i+1;
-                
-                if(next==n || prime(s.charAt(next))){
-                    r=(r+solve(s,k-1,len,next,t))%MOD;
+        long[][] t=new long[k+1][n+1];
+        t[0][0]=1;
+        
+        for(int i=1;i<k+1;i++){
+            long r=0;
+            for(int j=minLength*i;j<n+1;j++){
+                int prevcut=j-minLength;
+                if(isCut[prevcut]){
+                    r=(r+t[i-1][prevcut])%MOD;
+                }
+                if(isCut[j]){
+                    t[i][j]=r;
                 }
             }
         }
-        return t[k][idx]=(int)r;
+
+        return (int) t[k][n];
     }
     public boolean prime(char c){
         return c=='2'||c=='5'||c=='3'||c=='7';
