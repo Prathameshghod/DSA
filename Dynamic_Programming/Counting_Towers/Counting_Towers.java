f9910be35f61d44e1f41326aee3886f2f6e90c17
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-08-04 09:37:31 +0530

    Add Java solution for Counting_Towers

--- Dynamic_Programming/Counting_Towers/Counting_Towers.java
+++ Dynamic_Programming/Counting_Towers/Counting_Towers.java
@@ -5,39 +5,43 @@
  * Date: 2026-08-04
  */
 
-/******************************************************************************
- 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
- 
-*******************************************************************************/
-import java.util.*;
 import java.io.*;
-public class Main
-{
-    public static final int MOD = 1000_000_007;
  
-    public static int func(int n){
-        int[][] t= new int[n+1][2];
-        t[1][0]=1;
-        t[1][1]=1;
-        for(int i=2;i<n+1;i++){
-             t[i][0] = (int)(((long)4 * t[i - 1][0] + t[i - 1][1]) % MOD);
-            t[i][1] = (int)(((long)2 * t[i - 1][1] + t[i - 1][0]) % MOD);
-        }
-        return (int)((long) t[n][0]+(long) t[n][1])%MOD;
-    }
-    
+public class Main {
+ 
+    static final int MOD = 1_000_000_007;
+ 
     public static void main(String[] args) throws Exception {
  
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
         int tc = Integer.parseInt(br.readLine());
  
-        while (tc-- > 0) {
-            int n = Integer.parseInt(br.readLine());
-            System.out.println(func(n));
+        int[] q = new int[tc];
+        int max = 0;
+ 
+        for (int i = 0; i < tc; i++) {
+            q[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, q[i]);
         }
+ 
+        long[][] dp = new long[max + 1][2];
+ 
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+ 
+        for (int i = 2; i <= max; i++) {
+            dp[i][0] = (4 * dp[i - 1][0] + dp[i - 1][1]) % MOD;
+            dp[i][1] = (dp[i - 1][0] + 2 * dp[i - 1][1]) % MOD;
+        }
+ 
+        StringBuilder sb = new StringBuilder();
+ 
+        for (int i = 0; i < tc; i++) {
+            int n = q[i];
+            sb.append((dp[n][0] + dp[n][1]) % MOD).append("\n");
+        }
+ 
+        System.out.print(sb);
     }
 }
