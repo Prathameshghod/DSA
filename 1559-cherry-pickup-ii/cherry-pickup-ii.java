ec19abb551e2a6a0b2975ee52fc824b8512a9f7e
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-08-07 11:06:32 +0530

    Time: 21 ms (92.20%) | Memory: 46.5 MB (36.10%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -1,41 +1,62 @@
 class Solution {
-    public int solve(int[][] a,int r,int c1,int c2,int[][][] t){
-        int n=a.length;
-        int m=a[0].length;
 
-        if(c1<0 || c1>=m || c2<0 || c2>=m){
-            return -1000000;
+    int n;
+    int m;
+
+    int [][][] dp;
+    public int cherryPickup(int[][] grid) {
+       n = grid.length;
+       m = grid[0].length;
+
+       dp = new int [n][m][m];
+
+       for(int [][]arr:dp){
+        for(int [] nums:arr){
+            Arrays.fill(nums,-1);
         }
-        if(r==n){
-            return 0;
+       }
+
+
+       return (solve(0,0,m-1,grid));
+        
+    }
+
+    int solve(int r,int c1,int c2,int[][] grid){
+
+        if(c1<0 || c2<0 ||   c1>=m || c2>=m) return Integer.MIN_VALUE;
+
+       
+
+        if(r==n-1){
+            
+            if(c1==c2) return grid[r][c1];
+
+            return grid[r][c1] + grid[r][c2];
         }
-        if(t[r][c1][c2]!=-1){
-            return t[r][c1][c2];
-        }
-        int c=0;
-        if(c1==c2){
-            c=a[r][c1];
-        }
-        else{
-            c=a[r][c1]+a[r][c2];
-        }
-        int best=0;
+
+         if(dp[r][c1][c2] !=-1) return dp[r][c1][c2];
+        
+
+
+        int max = Integer.MIN_VALUE;
+
         for(int i=-1;i<=1;i++){
             for(int j=-1;j<=1;j++){
-                best=Math.max(best,solve(a,r+1,c1+i,c2+j,t));
+                max = Math.max(max,solve(r+1,c1+i,c2+j,grid));
             }
         }
-        return t[r][c1][c2]=best+c;
-    }
-    public int cherryPickup(int[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int[][][] t= new int[n+1][m+1][m+1];
-        for(int[][] i:t){
-            for(int[] j: i){
-                Arrays.fill(j,-1);
-            }
+
+        int curr;
+
+        if(c1==c2){
+            curr = grid[r][c1];
+        }else{
+            curr = grid[r][c1] + grid[r][c2];
         }
-        return solve(grid,0,0,m-1,t);
+      
+
+      return dp[r][c1][c2] = curr + max;
+
+
     }
 }
