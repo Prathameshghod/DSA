ade72009e14d2a13d5babc3b3e8e747a7f119c13
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-08-08 19:32:17 +0530

    Time: 16 ms (68.57%) | Memory: 46.9 MB (35.01%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,13 +2,13 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         int[][] t= new int[n+1][amount+1];
-
         for(int j=1;j<amount+1;j++){
             t[0][j]=Integer.MAX_VALUE-1;
         }
+       
         for(int i=1;i<n+1;i++){
             for(int j=1;j<amount+1;j++){
-            if(coins[i-1]<=j){
+                if(coins[i-1]<=j){
                     t[i][j]=Math.min(1+t[i][j-coins[i-1]],t[i-1][j]);
                 }
                 else{
@@ -16,6 +16,6 @@ class Solution {
                 }
             }
         }
-        return t[n][amount] ==Integer.MAX_VALUE-1 ? -1 :  t[n][amount];
+        return t[n][amount]==Integer.MAX_VALUE-1 ? -1 : t[n][amount];
     }
 }
