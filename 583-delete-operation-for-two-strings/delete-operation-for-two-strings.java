debbbd865d894929c436460cd34b207759b94280
Prathamesh Ghodmare <144260797+Prathameshghod@users.noreply.github.com>
2026-08-08 21:53:58 +0530

    Time: 7 ms (69.30%) | Memory: 46.5 MB (32.32%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -3,13 +3,6 @@ class Solution {
         int n=word1.length();
         int m=word2.length();
         int[][] t= new int[n+1][m+1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<m+1;j++){
-                if(i==0 || j==0){
-                    t[i][j]=0;
-                }
-            }
-        }
         for(int i=1;i<n+1;i++){
             for(int j=1;j<m+1;j++){
                 if(word1.charAt(i-1)==word2.charAt(j-1)){
@@ -20,34 +13,6 @@ class Solution {
                 }
             }
         }
-        StringBuilder s= new StringBuilder();
-        int i=n;
-        int j=m;
-        while(i>0 && j>0){
-            if(word1.charAt(i-1)==word2.charAt(j-1)){
-                s.append(word1.charAt(i-1));
-                i--;
-                j--;
-            }
-            else if(t[i-1][j]>t[i][j-1]){
-                s.append(word1.charAt(i-1));
-                i--;
-            }
-            else{
-                s.append(word2.charAt(j-1));
-                j--;
-            }
-        }
-        while (i > 0) {
-            s.append(word1.charAt(i - 1));
-            i--;
-        }
-        
-        while (j > 0) {
-            s.append(word2.charAt(j - 1));
-            j--;
-        }
-        
-        return s.length()-t[n][m];
+        return n+m-(2*t[n][m]);
     }
 }
